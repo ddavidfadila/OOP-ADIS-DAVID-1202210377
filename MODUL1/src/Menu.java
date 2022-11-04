c9dafb390cfa1fab92f3cd4bbe60d4ee55de93cd
ddavidfadila <115241668+ddavidfadila@users.noreply.github.com>
2022-11-04 08:07:08 +0700

    Add files via upload

---
+++ MODUL1/src/Menu.java
@@ -0,0 +1,11 @@
+public class Menu {
+    
+    public static void Menu(string[] args {
+        this.Menu = "Bakso"
+
+    }
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+
+}
---
+++ MODUL1/src/User.java
@@ -0,0 +1,20 @@
+public class User {
+
+    public String nama;
+    public Int nophone;
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+    public void setnama( string nama ){
+        this.nama = nama ;
+    
+    }
+
+   public void setnophone( int nophone){
+        this.nophone = nophone;
+        
+   }
+ 
+
+}
